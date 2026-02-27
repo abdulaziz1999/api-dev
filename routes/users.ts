4ee51dfc896921ebc828108d74c4a10d0ef6c277
Abdul Aziz <azizmentor96@gmail.com>
2026-02-27 08:18:39 +0700

    crud users

--- routes/users.ts
+++ routes/users.ts
@@ -6,6 +6,10 @@ const router = Router();
 
 // Basic CRUD routes
 router.get('/', authenticateToken, UserController.getAllUsers);
+router.get('/:id', authenticateToken, UserController.getUserById);
+router.post('/', authenticateToken, UserController.createUser);
+router.put('/:id', authenticateToken, UserController.updateUser);
+router.delete('/:id', authenticateToken, UserController.deleteUser);
 router.get('/with-relations', UserController.getUsersWithRelations);
 router.get('/search', UserController.searchUsers);
 router.get('/advanced', UserController.advancedQuery);
